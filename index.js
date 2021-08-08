195a9d679ffcd01b545dcff212513c8a036d4c31
Mustafa Ali <mustafamire@gmail.com>
2021-08-08 15:23:34 +0100

    Add: inquirer prompt to generate questions

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
+//node modules
 const inquirer = require('inquirer');
 const fs = require('fs');
+
+// inquirer to generate questions
+inquirer.prompt([
+    {
+        type: "input",
+        messaage: "what is your project title?",
+        name: "tittle",
+        //validate property to check user has provided a value
+        validate: (value) => { if (value) { return true } else { return "I need a value to continue" } },
+
+    }
+]
+
+
+)
