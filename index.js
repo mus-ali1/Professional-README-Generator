96caa18394d8079cb9b98af29147a5e76c741ae9
Mustafa Ali <mustafamire@gmail.com>
2021-08-08 21:33:34 +0100

    Add: template for ReadMe file /function to create the template in ReadMe file

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 //node modules
 const inquirer = require('inquirer');
 const fs = require('fs');
+let path = './README.md';
+let $template = "";
+
 
 // inquirer to generate questions
 inquirer.prompt([
@@ -33,38 +36,39 @@ inquirer.prompt([
     {
         type: "input",
         message: "Any instructions and examples of usage ?",
-        name: "installation",
-        //validate property to check user has provided a value
-        validate: (value) => { if (value) { return true } else { return "I need a value to continue" } },
-
-
-    },
-    {
-        type: "input",
-        message: "Any credits ?",
-        name: "installation",
-        //validate property to check user has provided a value
-        validate: (value) => { if (value) { return true } else { return "I need a value to continue" } },
-
-
-
-
-    },
-    {
-        type: "input",
-        message: "Any instructions and examples of usage ?",
         name: "usage",
         //validate property to check user has provided a value
         validate: (value) => { if (value) { return true } else { return "I need a value to continue" } },
 
-    }, {
 
-        type: "list",
-        message: "What licence did you use ?",
-        name: "licence",
-        choices: ["MIT licence, Apache Licence, GPL licence, N/A"],
+    },
+    {
+        type: "input",
+        message: "Any credits/ collaborators ?",
+        name: "credits",
         //validate property to check user has provided a value
         validate: (value) => { if (value) { return true } else { return "I need a value to continue" } },
+
+
+
+
+    },
+    {
+
+        type: "list",
+        message: "What licence is relevant to this project?",
+        name: "licence",
+        choices: ["MIT", "Apache", "GPLv3", "Other", "N/A"],
+        //validate property to check user has provided a value
+        validate: (value) => { if (value) { return true } else { return "I need a value to continue" } },
+    },
+    {
+        type: "input",
+        message: "How can others contribute?",
+        name: "contribute",
+        //validate property to check user has provided a value
+        validate: (value) => { if (value) { return true } else { return "I need a value to continue" } },
+
     },
     {
 
@@ -83,9 +87,46 @@ inquirer.prompt([
         //validate property to check user has provided a value
         validate: (value) => { if (value) { return true } else { return "I need a value to continue" } },
 
+    }, {
+        type: "input",
+        message: "Enter your image url (https://github.com/your-repository/...)",
+        name: "image",
+
     }
+])
+    // Receives response from questions
+    .then(function (response) {
+        // Creates the template for the readme
+        let licenceInfo = "";
+        $template += `# ${response.title}\n\n`;
 
-]
+        if (response.licence === "MIT") {
+            $template += `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)\n\n`;
+            licenceInfo = '(https://opensource.org/licenses/MIT)\n\nYou have the freedom to do as you like with this permissive software, as long as an original copy and license notice is included. I can not be held liable for this software.\n\n';
+        } else if (response.licence === "Apache") {
+            $template += `[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)\n\n`;
+            licenceInfo = '(http://www.apache.org/licenses/LICENSE-2.0.html)\n\nYou have the freedom to do as you like with this permissive software. This license also contains a patent license from the contributors of the code.\n\n';
+        } else if (response.licence === "GPLv3") {
+            $template += `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)\n\n`;
+            licenceInfo = '(http://www.gnu.org/licenses/gpl-3.0.html)\n\nYou have the freedom to run, study, share, and modify this permissive software. Anyone who acquires this software must make it available to anyone else under the same licensing agreement.\n\n';
+        };
 
 
-)
+        $template += `### Table of Contents\n\n- [Description](#description)\n- [Installation](#installation)\n- [Usage](#usage)\n- [Contributing](#contributing)\n- [Testing](#testing)\n- [Questions](#questions)\n- [License](#license)\n- [Application Image](#application-image)\n\n`;
+        $template += `## Description\n\n${response.description}\n\n`;
+        $template += `## Installation\n\n${response.installation}\n\n`;
+        $template += `## Usage\n\n${response.usage}\n\n`;
+        $template += `## Credits\n\n${response.credits}\n\n`;
+        $template += `## Questions\n\nIf you have any questions feel free to contact me here:\n\n ##### Github: [github.com/${response.username}](https://github.com/${response.username})\n\n ##### Email: [${response.email}](mailto:${response.email}?subject=[GitHub])\n\n`;
+        $template += `## License\n\n[${response.licence}]${licenceInfo}`;
+        $template += `## Contributing\n\n${response.contribute}\n\n`;
+        $template += `## Application Image\n\n ![Image of Application](${response.image})`;
+
+
+        // Writes the created template to README.md file
+        fs.writeFile(path, $template, function (err) {
+            if (err) {
+                console.log(err);
+            }
+        })
+    });
