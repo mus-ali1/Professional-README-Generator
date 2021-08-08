30582157f4ea00f33991840d5c41a16f8da2b603
Mustafa Ali <mustafamire@gmail.com>
2021-08-08 15:50:13 +0100

    Add: prompts for title, description etc

--- index.js
+++ index.js
@@ -6,10 +6,59 @@ const fs = require('fs');
 inquirer.prompt([
     {
         type: "input",
-        messaage: "what is your project title?",
-        name: "tittle",
+        message: "what is your project title ?",
+        name: "title",
         //validate property to check user has provided a value
         validate: (value) => { if (value) { return true } else { return "I need a value to continue" } },
+
+    },
+    {
+        type: "input",
+        message: "Please provide a short decription of your project ?",
+        name: "description",
+        //validate property to check user has provided a value
+        validate: (value) => { if (value) { return true } else { return "I need a value to continue" } },
+
+
+    },
+    {
+        type: "input",
+        message: "What steps are required to install your project ?",
+        name: "installation",
+
+        //validate property to check user has provided a value
+        validate: (value) => { if (value) { return true } else { return "I need a value to continue" } },
+
+    },
+    {
+        type: "input",
+        message: "Any instructions and examples of usage ?",
+        name: "installation",
+        //validate property to check user has provided a value
+        validate: (value) => { if (value) { return true } else { return "I need a value to continue" } },
+
+
+    },
+    {
+        type: "input",
+        message: "Any credits ?",
+        name: "installation",
+        //validate property to check user has provided a value
+        validate: (value) => { if (value) { return true } else { return "I need a value to continue" } },
+
+
+
+
+    },
+    {
+        type: "input",
+        message: "Any instructions and examples of usage ?",
+        name: "installation",
+        //validate property to check user has provided a value
+        validate: (value) => { if (value) { return true } else { return "I need a value to continue" } },
+
+
+
 
     }
 ]
